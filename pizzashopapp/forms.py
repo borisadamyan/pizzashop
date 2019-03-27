88172bce3c73f8adee4d8d288f14a6b002266a1a
Boris Adamyan <boris.adamyan@gmail.com>
2019-03-27 14:35:08 +0400

    pizza shop app creating

---
+++ pizzashopapp/forms.py
@@ -0,0 +1,30 @@
+from django import forms
+from django.contrib.auth.models import User
+from pizzashopapp.models import PizzaShop, Pizza
+
+
+class UserForm(forms.ModelForm):
+    username = forms.CharField(max_length=30, required=True)
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'first_name', 'last_name', 'email')
+
+
+class UserFormForEdit(forms.ModelForm):
+    email = forms.CharField(max_length=30, required=True)
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+class PizzaShopForm(forms.ModelForm):
+    class Meta:
+        model = PizzaShop
+        fields = ('name', 'phone', 'address', 'logo')
+
+
+class PizzaForm(forms.ModelForm):
+    class Meta:
+        model = Pizza
+        exclude = ('pizzashop',)
